17c248a88ba65900280972361ae0ec582b9b58f5
yupuli <49184904+yupuli@users.noreply.github.com>
2019-08-16 09:25:11 +1000

    Initial commit

---
+++ FIT5032_Week03/Exercise/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week03.Exercise
+{
+    public class Student
+    {
+        public string Name
+        {
+            get; set;
+        }
+        public string PhoneNumber
+        {
+            get; set;
+        }
+        public Student(String name, String phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+        public class ExampleDictionary
+        {
+            public void Example()
+            {
+                Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+                Student s1 = new Student("James", "0426568971");
+                Student s2 = new Student("Karsa", "0426531785");
+
+                studentDictionary.Add(1, s1);
+                studentDictionary.Add(2, s2);
+
+                Student result = new Student("", "");
+
+                studentDictionary.TryGetValue(1, out result);
+            }
+        }
+    }
+}
